9b68e8a6ea88e88a9c6f7c30d0d7f37113875a33
shanyingbo <shanyingbo@xingshulin.com>
2018-12-08 20:44:23 +0800

    Initial commit

---
+++ ls1.c
@@ -0,0 +1,24 @@
+//
+// Created by 单颖博 on 2018/12/8.
+//
+
+#include <apue.h>
+#include <dirent.h>
+
+int main(int argc, char *argv[]) {
+    DIR *dp;
+    struct dirent *dirp;
+
+    if (argc != 2)
+        err_quit("usage: ls directory_name");
+
+    if ((dp = opendir(argv[1])) == NULL)
+        err_sys("can't open %s", argv[1]);
+    while ((dirp = readdir(dp)) != NULL)
+        printf("%s\n", dirp->d_name);
+
+    closedir(dp);
+
+    return 0;
+}
+
